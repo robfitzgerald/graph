17b3d6b828f8206487c35609ca9f29c7e352a362
Rob Fitzgerald <robfitzgerald@gmail.com>
2018-04-28 13:42:27 -0600

    now stores the state that results from the simulation if it has a globally optimal cost.

---
+++ generator/ops/GraphTopology.scala
@@ -0,0 +1,26 @@
+//package cse.fitzgero.graph.generator.ops
+//
+//import cse.fitzgero.graph.propertygraph._
+//
+//trait GraphTopology[G] extends PropertyGraph with PropertyGraphOps with PropertyGraphMutationOps[GraphTopology[G]] {
+//  self: {
+//    type VertexId = String
+//  } =>
+//  def empty: GraphTopology[G]
+//  def topologySuffix: VertexId
+//  def defaultEdge: Edge
+//  def defaultVertex: Vertex
+//}
+//
+//object GraphTopology {
+//  def square[G](n: Int, g: GraphTopology[G]): GraphTopology[G] = {
+//    val vertices = for {
+//      i <- 0 to n
+//      j <- 0 to n
+//    } yield (s"$g.topologySuffix-$i-$j", g.defaultVertex)
+//    val graphVertices = vertices.foldLeft(g.empty){
+//      (acc, vertex) =>
+//        acc.updateVertex(vertex._1, vertex._2)
+//    }
+//  }
+//}
